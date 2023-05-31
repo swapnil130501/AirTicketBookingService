5508a10b8a77c7661cdc68fce757e344c8de8841
swapnil <swapnil130501@gmail.com>
2023-05-31 13:22:25 +0530

    first commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const { PORT } = require('./config/server-config');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const setupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`server started on port ${PORT}`);
+
+        if(process.env.DB_SYNC) {
+            db.Sequelize.sync({alter: true});
+        }
+    });
+}
+
+setupAndStartServer();
